4abd10b269b416f8f01f96b1b072bd4060ce08a9
Kun Wang <ifreedom.cn@gmail.com>
2016-04-27 00:18:22 +0800

    Add empty ReaderViewController.

---
+++ xComic/ReaderViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReaderViewController.swift
+//  xComic
+//
+//  Created by Kun Wang on 4/27/16.
+//  Copyright © 2016 Kun Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ReaderViewController: UITableViewController {
+}
